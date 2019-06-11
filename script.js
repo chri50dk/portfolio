6de8f7a362df45586aa0e7503e96e4f5ee132a12
chri50dk <chri50dk@stud.kea.dk>
2019-06-11 23:34:48 +0200

    CV content skrevet færdigt. Placeholder styling. HTML skeleter til alle .html'er

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ window.addEventListener("load", sidenVises);
 function sidenVises() {
     console.log("siden vises");
     document.querySelector("#mere").addEventListener("click", readMore);
+    document.querySelector(".menuknap").addEventListener("click", openNav);
+    document.querySelector(".luknav").addEventListener("click", closeNav);
 }
 
 function readMore() {
     console.log("read more");
     document.querySelector("#box").classList.toggle("stor");
     document.querySelector("#box").classList.toggle("lille");
+
+
+}
+
+function openNav() {
+    console.log("open nav");
+    document.querySelector("#sidenavid").classList.add("show");
+}
+
+function closeNav() {
+    console.log("close nav");
+    document.querySelector("#sidenavid").classList.remove("show");
+
 }
